9b702f8a3a90d842526694c0c90c31dc7945c4a8
NadeemAhmadd <nadeem_ahmad@hotmail.ca>
2023-05-09 02:11:53 -0400

    inserting Project section

--- nadeems_porfolio_site/src/App.js
+++ nadeems_porfolio_site/src/App.js
@@ -1,95 +1,126 @@
-import './App.css';
-import Backround from './components/backround';
-import './components/aurora.css'
-import './components/typing.css'
+import "./App.css";
+import Backround from "./components/backround";
+import "./components/aurora.css";
+import "./components/typing.css";
 import { useRef } from "react";
 import "./App.css";
-import TopPageButton from './components/top_page_button';
-import MenuBar from './components/MenuBar';
-import PictureBox from './components/PictureBox';
-import AboutBar from './components/AboutBar';
+import TopPageButton from "./components/top_page_button";
+import MenuBar from "./components/MenuBar";
+import PictureBox from "./components/PictureBox";
+import AboutBar from "./components/AboutBar";
+import Project_button from "./components/project_button";
 
 function App() {
   const aboutRef = useRef(null);
   const projectRef = useRef(null);
   const homeRef = useRef(null);
 
-
   return (
-    
     <div class="content fade-in">
-    
+      <section className="section1 content" id="titlepage" ref={homeRef}>
+        <Backround id="Canvas1" />
 
-    <section className='section1 content' id="titlepage" ref={homeRef} > 
-    <Backround id="Canvas1"/>
+        <h1
+          class="title  cursor typewriter-animation"
+          style={{ marginTop: "350px" }}
+        >
+          Hello, My name is Nadeem.
+          <div class="aurora">
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+            <div class="aurora__item"></div>
+          </div>
+        </h1>
+        <p
+          class="cursor typewriter-animation"
+          style={{ fontSize: "16px", animationDelay: "6s" }}
+        >
+          Welcome to my portfolio site.
+        </p>
+        <TopPageButton />
+        <MenuBar
+          aboutRef={aboutRef}
+          homeRef={homeRef}
+          projectRef={projectRef}
+        />
+      </section>
+      <section className="section2" id="about" ref={aboutRef}>
+        <AboutBar />
+        {/* <Backround id="Canvas2"/> */}
 
-    <h1  class="title  cursor typewriter-animation" style={{ marginTop: '350px' }}>Hello, My name is Nadeem.
-        <div class="aurora">
-        <div class="aurora__item"></div>
-        <div class="aurora__item"></div>
-        <div class="aurora__item"></div>
-        <div class="aurora__item"></div>
+        <h3
+          class="section2-heading about__heading"
+          style={{ marginTop: "0px" }}
+        >
+          About
+        </h3>
+        <img
+          src={require("./images/html.png")}
+          style={{ marginRight: "700px", marginTop: "100px", width: "10%" }}
+        />
+        <p class="profile__blurb">
+          I'm a computer science graduate with a deep passion for coding. I
+          enjoy combining various elements, such as front and back-end
+          development, with machine learning and other cutting-edge coding
+          technologies to develop innovative projects. In addition, I engage in
+          extensive research in the field of combinatorics, where I create
+          algorithms to tackle complex big data problems. When I'm not at my
+          computer I like to spend my time reading, going to the gym and
+          traveling.
+        </p>
 
-
+        <div style={{ marginLeft: "700px", marginTop: "-375px" }}>
+          <div class="picture-box-container">
+            <PictureBox image={require("./images/keras.png")} />
+            <PictureBox image={require("./images/python.png")} />
+            <PictureBox image={require("./images/tensorFlow.png")} />
+            <PictureBox image={require("./images/javaScript.png")} />
+          </div>
+          <div class="picture-box-container">
+            <PictureBox image={require("./images/react.png")} />
+            <PictureBox image={require("./images/java.png")} />
+            <PictureBox image={require("./images/C.png")} />
+          </div>
+          <div class="picture-box-container">
+            <PictureBox image={require("./images/kotlin.png")} />
+            <PictureBox image={require("./images/aws.png")} />
+            <PictureBox image={require("./images/git.png")} />
+            <PictureBox image={require("./images/firebase.png")} />
+          </div>
         </div>
+      </section>
 
-    </h1>
-    <p class="cursor typewriter-animation"style={ {fontSize: '16px', animationDelay: '6s'}}>Welcome to my portfolio site.</p>
-    <TopPageButton/>   
-    <MenuBar
-        aboutRef={aboutRef}
-        homeRef={homeRef}
-        projectRef={projectRef}
-      />
-    </section>
-    <section className='section2' id="about" ref={aboutRef}>
-    <AboutBar/>
-    {/* <Backround id="Canvas2"/> */}
+      <section className="container section3" id="project" ref={projectRef}>
+        <h3
+          class="section2-heading project__heading"
+          style={{ marginTop: "0px" }}
+        >
+          Projects
+        </h3>
 
-    <h3 class="section2-heading about__heading" style={{ marginTop: '0px' }}>About</h3>
-    <img src={require('./images/html.png')} style={{ marginRight: '700px',marginTop: '100px' , width: '10%' }}/>
-    <p class="profile__blurb">
-
-    I'm a computer science graduate with a deep passion for coding.
-    I enjoy combining various elements, such as front and back-end development, with machine learning and 
-    other cutting-edge coding technologies to develop innovative projects. In addition, I engage in extensive research in 
-    the field of combinatorics, where I create algorithms to tackle complex big data problems.
-    When I'm not at my computer I like to spend my time reading, going to the gym and traveling.
-
-    </p>
-    
-    <div style={{ marginLeft: '700px',marginTop: '-375px'  }}>
-    <div class="picture-box-container">
-      <PictureBox image={require('./images/keras.png')}/>
-      <PictureBox image={require('./images/python.png')}/>
-      <PictureBox image={require('./images/tensorFlow.png')}/>
-      <PictureBox image={require('./images/javaScript.png')}/>
+        <div className="check-shift">
+          <div className="projectbox left-align-project">
+            <div className="project-description-align">
+              <h3 className="neuroclassify__heading">NeuroClassify</h3>
+              <small className="project-description-align">
+                Mri tumor detection and classification with deep learning.
+                Built with Python, TensorFlow, Keras, sckit-learn and Matplotlib.
+              </small>
+              <Project_button/>
+              <img
+                className="project_img"
+                src={require("./gifs/testing.gif")}
+                alt=""
+                width="400"
+                height="300"
+                style={{ float: "right" }}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-    <div class="picture-box-container">
-      <PictureBox image={require('./images/react.png')}/>
-      <PictureBox image={require('./images/java.png')}/>
-      <PictureBox image={require('./images/C.png')}/>
-    </div>
-    <div class="picture-box-container">
-      <PictureBox image={require('./images/kotlin.png')}/>
-      <PictureBox image={require('./images/aws.png')}/>
-      <PictureBox image={require('./images/git.png')}/>
-      <PictureBox image={require('./images/firebase.png')}/>
-    </div>
-    </div>
-    
-
-
-
-
-    </section>
-
-
-    
-    </div>
-
   );
-  
 }
-
 export default App;
---
+++ nadeems_porfolio_site/src/components/project_button.js
@@ -0,0 +1,11 @@
+
+import './project_button.css';
+
+function Project_button (){
+    
+    return (
+<a href="#">Hover this link</a>
+    );
+  }
+
+export default Project_button;
